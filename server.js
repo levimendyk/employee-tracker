ff35c582dc0fceb28caf6197e656a26e2eb999a2
levimendyk <levi3743@gmail.com>
2022-09-14 13:17:55 -0600

    Added db.query to retrieve information from the seeds database

--- server.js
+++ server.js
@@ -2,46 +2,76 @@ const mysql2 = require("mysql2");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
-const db = mysql2.createConnection(
-  {
-    host: "localhost",
-    user: "root",
-    password: "rootroot",
-    database: "employees_db",
-  },
-);
+const db = mysql2.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "rootroot",
+  database: "employees_db",
+});
 
 db.connect(function (err) {
-    if(err) throw err;
-    console.log(`Connected to the employees_db database.`)
-    mainQuestion()
-})
+  if (err) throw err;
+  console.log(`Connected to the employees_db database.`);
+  mainQuestion();
+});
 
 function mainQuestion() {
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'mainQuestion',
-            message: 'What would you like to do?',
-            choices: ['View All', 'View Employees', 'View Roles', 'View Departments', 'Add Employees', 'Add Roles', 'Add a Department', 'Done']
-        }
-    ]).then(answer => {
-        if(answer.mainQuestion === 'View All') {
-            viewAll()
-        } else if (answer.mainQuestion === 'View Employees') {
-            viewEmployees()
-        } 
-    
-        // else {
-            // db.end()
-        // }
-    })
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "mainQuestion",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Department",
+          "Done",
+        ],
+      },
+    ])
+    .then((answer) => {
+      if (answer.mainQuestion === "View All Employees") {
+        viewAllEmployees();
+      } else if (answer.mainQuestion === "Add Employee") {
+        addEmployee();
+      } else if (answer.mainQuestion === "View All Roles") {
+        viewAllRoles();
+      } else if (answer.mainQuestion === "Add Role") {
+        addRole();
+      } else if (answer.mainQuestion === "View All Departments") {
+        viewAllDepartments();
+      } else if (answer.mainQuestion === "Add Department") {
+        addDepartment();
+      } else {
+        db.end();
+      }
+    });
 }
 
-function viewAll() {
-
+function viewAllEmployees() {
+  db.query("SELECT * FROM employee", function (err, results) {
+    console.log(results);
+  });
 }
 
-function viewEmployees() {
+function addEmployee() {}
 
+function viewAllRoles() {
+  db.query("SELECT * FROM role_position", function (err, results) {
+    console.log(results);
+  });
 }
+
+function addRole() {}
+
+function viewAllDepartments() {
+  db.query("SELECT * FROM department", function (err, results) {
+    console.log(results);
+  });
+}
+
+function addDepartment() {}
